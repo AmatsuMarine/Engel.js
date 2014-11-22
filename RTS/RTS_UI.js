a0a357117ba8b0a8fdf572270e5ae6b04b6fbcad
Matthew Marine <AmatsuMarine@gmail.com>
2014-11-21 23:25:14 -0500

    2D GUI elements drawn with solid color

---
+++ RTS/RTS_UI.js
@@ -0,0 +1,28 @@
+function RTS_UI(){
+
+	this.minimapFrame;
+	this.commandCardFrame;
+	this.resourceFrame;
+
+	this.draw = function(){
+		if(!minimapFrame){
+			init();
+			debug.log("RTS_UI null");
+		}
+
+		minimapFrame.draw();
+		commandCardFrame.draw();
+		resourceFrame.draw();
+	}
+
+	var init = function(){
+		minimapFrame = new GUI_Texture([-1.0,-0.25,0.65,0.75]);
+		commandCardFrame = new GUI_Texture([0.5,-0.35,1.5,0.65]);
+		resourceFrame = new GUI_Texture([0.65,1.0,0.35,0.15]);
+
+		debug.log("create RTS_UI");
+	}
+
+	init();
+	
+}
